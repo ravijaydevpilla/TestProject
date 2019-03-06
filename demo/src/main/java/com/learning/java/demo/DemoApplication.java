108bcb0c65c89cdcc11d9c0afc396fab73d37967
Ravi Jaydev Pilla <32315258+ravijaydevpilla@users.noreply.github.com>
2019-03-06 16:37:59 -0500

    updated sysout

--- demo/src/main/java/com/learning/java/demo/DemoApplication.java
+++ demo/src/main/java/com/learning/java/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Spring Boot Application has started......");
+		System.out.println("Spring Boot Application has started");
 	}
 
 }
