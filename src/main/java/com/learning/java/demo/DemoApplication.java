1aad396aeb1bed4594414d280dbffd26c91ac540
Ravi Jaydev Pilla <jaydev.ravi@gmail.com>
2019-03-08 13:10:56 -0500

    changes

--- src/main/java/com/learning/java/demo/DemoApplication.java
+++ src/main/java/com/learning/java/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Spring Boot Application has started");
+		//testing
 	}
 
 }
